0a38d50f535d63a606f9f2d1eb4de34e525eb68f
unknown <smartdesk2015@gmail.com>
2023-09-11 12:57:06 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app= express()
+import Connection from "./Database/db.js";
+import dotenv from "dotenv"
+dotenv.config()
+app.get("/",(req,res)=>{
+    res.send("everything is fine")
+})
+
+const PORT= process.env.PORT
+app.listen(PORT, async(req,res)=>{
+    try {
+         await Connection
+         console.log("connected to databse")
+    } catch (error) {
+        console.log(error.message)
+    }
+    console.log(`server is awake at port ${PORT}`)
+})
+
