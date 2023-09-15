53ee25bf2ac7c48bf6ad3d26c664dc24c610bc7f
parimal paramanik <115460386+parimal-paramanik@users.noreply.github.com>
2023-09-15 19:02:50 +0530

    Update route.js

--- server/Routes/route.js
+++ server/Routes/route.js
@@ -1,7 +1,7 @@
 import express from "express"
 const Router= express.Router()
 import { Signup ,Login} from "../Controller/usercontroller.js"
-
+// defining all the routes
 Router.post("/signup",Signup)
 Router.post("/login",Login)
 
