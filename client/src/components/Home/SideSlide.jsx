2065fe090fc93a5fb5c3c7483031bc6918cf617c
unknown <smartdesk2015@gmail.com>
2023-09-23 22:33:06 +0530

    image styling

--- client/src/components/Home/SideSlide.jsx
+++ client/src/components/Home/SideSlide.jsx
@@ -1,19 +1,37 @@
 
-import { Box } from "@mui/material"
+import { Box ,styled} from "@mui/material"
 import Slide from "./Slide"
 // styles will be here 
+const Leftslide= styled(Box)`
+    width:82%;
+`
+const Rightslide= styled(Box)`
+width: 16%;
+padding :5px ;
+margin-top :10px;
+margin-left :20px;
+
+
+
+`
+const Component = styled(Box)`
+  display:flex;
+`
 
 const SideSlide = ({ products, title, watch }) => {
-    return (
-        <Box>
+    const sideimgUrl ="https://rukminim1.flixcart.com/fk-p-flap/530/810/image/d663cbddbbcb4ef6.jpg?q=20"
 
-            <Box>
+    return (
+        <Component>
+
+            <Leftslide>
                 <Slide  products= {products} title= {title} watch= {watch}/>
-            </Box>
+            </Leftslide>
               
-            <Box>
-            </Box>
-        </Box>
+            <Rightslide>
+                <img src= {sideimgUrl}  alt="sideimg" style={{width: 240}}/>
+            </Rightslide>
+        </Component>
     )
 }
 
